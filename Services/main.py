22d8b86a882f7d723097131466b481fc367fadd2
Javier20105 <xavierescobar3335@gmail.com>
2017-09-01 18:10:01 -0300

    Se corrigio el codigo
    se revisaron que los nombre de variables sean relevantes
    se modifico el orden de los condicionales para no hacer operaciones de
    mas
    se corrigio el error de que se muestren muchas ventanas

--- Services/main.py
+++ Services/main.py
@@ -22,20 +22,18 @@ def main():
             cv2.circle(imagen,punto90,1, (0, 255, 0), 5)
             cv2.circle(imagen,extremoDerecho,1, (0, 255, 0), 5)
             
-            if(x>=320):
-                dCamaraCabeza =  Calculador.calcularDistanciaCM(180,40,x-320)
+            if validadorDesp.validarDesplazamiento((x,y)):
+                desplazamiento =  Calculador.calcularDistanciaCM(180,40,x-320)
                 dCabezaMano = 100
-                #print "distancia: "+str(dCamaraCabeza)
                 
-                if validadorDesp.validarDesplazamiento((x,y)):
+                if (x>=320) :
 
-                    Calculador.calcularDistanciaPuntos(punto90,extremoDerecho)
-                    hipotenusa = Calculador.calcularDistanciaLados(dCamaraCabeza,dCabezaMano)
+                    hipotenusa = Calculador.calcularDistanciaLados(desplazamiento,dCabezaMano)
                     angulo = Calculador.calcularAngulo(dCabezaMano,hipotenusa)
-                    print "x"+str(angulo)
+                    print "x "+str(angulo)
                     comunicador.enviarDatos("x"+str(angulo))
 
-        imshow("WebCam", imagen)
+        imshow("webcam", imagen)
        
         if waitKey(1) & 0xFF == ord('q'):
             break;
